840f99151f5ef203ce1e57397653a1f90b49b1d4
QuanVoltmeter1008 <77684073+QuanVoltmeter1008@users.noreply.github.com>
2021-01-19 23:42:02 +0800

    Add files via upload
    Add my own file.

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  8 21:42:51 2020
+
+@author: voltmeter
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
